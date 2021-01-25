ab23cd384bc326dbb9f80efd7a250454e7e46461
André Alves Santana <63176885+Andre-Santana@users.noreply.github.com>
2021-01-25 10:03:53 -0300

    Resolução da Questão Imãos e Três por dois da OBI
    Resolução da Questão Imãos e Três por dois da OBI (Olímpiada Brasileira de Informática).

--- Códigos Pascal/DESAFIO1.pas
+++ Códigos Pascal/DESAFIO1.pas
@@ -2,6 +2,13 @@ Program Pzim ;
 var
 n,m,diferenca,total:integer;
 Begin
+	//Resolução da questão "Irmãos" da OBI (Olímpiada Brasileira de Informática)
+	//Otávio tem dois irmãos, um mais velho (Orlando) e um mais novo do que ele (Oscar). As idades dos 
+	//três irmãos formam uma progressão aritmética: a diferença de idade dos dois irmãos mais novos 
+	//(Otávio e Oscar) é igual à diferença de idade dos dois irmãos mais velhos (Orlando e Otávio).
+	//Dadas as idades de Otávio e de seu irmão mais novo, escreva um programa para determinar a idade do 
+	//irmão mais velho.	
+	         
   writeln('Escreva a idade do irmão mais novo (Oscar): ');
   read(N);
   writeln('Escreva a idade de Otávio: ');
--- Códigos Pascal/DESAFIO2.pas
+++ Códigos Pascal/DESAFIO2.pas
@@ -3,6 +3,18 @@ var
 p,maior:array [1..3] of integer;
 i,n,total:integer;
 Begin
+	//Resolução da questão "Três por Dois" da OBI (Olímpiada Brasileira de Informática)
+	
+	//Uma loja de chocolates está fazendo uma promoção do tipo "leve três pague dois". Mais precisamente, 
+	//você pode escolher quaisquer três chocolates da loja e paga apenas pelos dois mais caros que 
+	//escolher, levando o mais barato gratuitamente. Se você levar mais do que três chocolates, você pode 
+	//agrupá-los em grupos de três e levar o chocolate mais barato de cada grupo gratuitamente.
+
+	//Por exemplo, se você escolher chocolates de preços (em reais) 8, 5, 10, 2, 5, 10 e 4, você pode 
+	//agrupá-los como (8,5,10), (2,5,10) e (4), pagará um total de (10+8) + (10+5) + 4, ou seja, 37 reais. Mas você pode agrupá-los de uma forma melhor e assim conseguir pagar menos. Você consegue ver como?
+
+	//Dados os preços dos chocolates escolhidos, escreva um programa para determinar o menor preço a pagar.
+	
   maior[1]:=-99999;
   i:=0;
   write('Escreva o número do chocolate: ');
